2c623f541422cf1b5917d32f794327ce11ec0171
devinsomniac <inzamamchowdhury20@gmail.com>
2025-01-12 00:07:04 +0600

    fix : Production Error

--- app/Users/[id]/page.tsx
+++ app/Users/[id]/page.tsx
@@ -1,32 +1,45 @@
-import CustomerSubscriptions from '@/components/CustomerSubscriptions'
-import Signature from '@/components/Signature'
-import { db } from '@/Database'
-import { customer, subscription } from '@/Database/schema'
-import { eq } from 'drizzle-orm'
-import React from 'react'
+import CustomerSubscriptions from '@/components/CustomerSubscriptions';
+import Signature from '@/components/Signature';
+import { db } from '@/Database';
+import { customer, subscription } from '@/Database/schema';
+import { eq } from 'drizzle-orm';
+import React from 'react';
+
 interface PageProps {
   params: {
     id: string;
   };
 }
 
-const page: React.FunctionComponent<PageProps> = async ({ params }) => {
-  const { id } = params;
+const Page: React.FunctionComponent<PageProps> = async ({ params }) => {
+  const { id } = await params; // Ensure `params` resolves properly.
   const customerId = id;
+
   console.log(customerId);
+
   if (!customerId) {
     throw new Error("No customer found");
   }
-  const customerResponse = await db.select().from(customer).where(eq(customer.customerId, customerId));
+
+  const customerResponse = await db
+    .select()
+    .from(customer)
+    .where(eq(customer.customerId, customerId));
+
   const customerDetails = customerResponse[0];
 
-  const customerSubscription = await db.select().from(subscription).innerJoin(customer, eq(subscription.customerId, customerId)).where(eq(customer.customerId, customerId));
+  const customerSubscription = await db
+    .select()
+    .from(subscription)
+    .innerJoin(customer, eq(subscription.customerId, customerId))
+    .where(eq(customer.customerId, customerId));
+
   console.log(customerSubscription);
 
   return (
-    <div className='p-8'>
-      <div className='w-[30vw] p-3 bg-slate-100 rounded-lg shadow-lg hover:bg-yellow-100 border border-black'>
-        <p className='font-bold text-3xl'>{customerDetails.name}</p>
+    <div className="p-8">
+      <div className="w-[30vw] p-3 bg-slate-100 rounded-lg shadow-lg hover:bg-yellow-100 border border-black">
+        <p className="font-bold text-3xl">{customerDetails.name}</p>
         <p>Customer id : {customerDetails.customerId}</p>
         <p>Customer Pan : {customerDetails.pan}</p>
       </div>
@@ -36,4 +49,4 @@ const page: React.FunctionComponent<PageProps> = async ({ params }) => {
   );
 };
 
-export default page;
+export default Page;
